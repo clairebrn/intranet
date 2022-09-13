ee8543073a41371869d438005ab217232af4051e
claire bernard <claire.bernard@bluesoft-group.com>
2022-09-13 19:06:26 +0200

    update css page d'accueil

--- client/src/views/Accueil.jsx
+++ client/src/views/Accueil.jsx
@@ -20,20 +20,27 @@ const Accueil = () => {
   return (
     <div className={style.accueil_container}>
       <div className={style.accueil_title}>
-        <h1>Hello xxx !!</h1>
-        <h2>Connais-tu xxx ?</h2>
+        <h1>Hello Bernardo !!</h1>
+        <p>Connais-tu machin ?</p>
       </div>
       <div className={style.card_container}>
         <div>
           <img src="https://picsum.photos/100/100" alt="" />
         </div>
-        <p>John Doe (35ans)</p>
+        <p>
+          <span>John Doe</span> (35ans)
+        </p>
         <p>Paris, France</p>
         <p>email</p>
         <p>01 02 03 04 05</p>
         <p>Anniversaire : 17 janvier</p>
       </div>
-      <button>Dire bonjour à quelqu'un d'autre</button>
+
+      <div className={style.btn_container}>
+        <button type="submit" className={style.shadow}>
+          Dire bonjour à quelqu'un d'autre
+        </button>
+      </div>
     </div>
   );
 };
